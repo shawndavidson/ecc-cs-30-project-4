bb1eaff6104ac942679802c2681352bcff9a1757
Shawn Davidson <shawnkdavidson@gmail.com>
2022-05-13 17:10:17 -0700

    Add stub for Ice

---
+++ IceMan/Ice.h
@@ -0,0 +1,16 @@
+#ifndef ICE_H_
+#define ICE_H_
+
+#include "Actor.h"
+class Ice : public Actor
+{
+public:
+	Ice() {};
+	virtual ~Ice() {}
+
+	virtual void doSomething() {
+		// TODO
+	}
+};
+
+#endif ICE_H_
